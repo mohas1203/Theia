ac73b5fdda88e69273c09f2e52d6b4a6a9abb883
Shaheed <mohas1203@wrdsb.ca>
2021-02-05 21:32:45 -0500

    Added bootstrap nav bar and fixed static files

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
 
-@app.route("/<name>")
-def user(name):
-    return f"hello {name}!"
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":

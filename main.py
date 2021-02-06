3eb3d50edfd2578f3bd873ee1dab62a29aec7ddb
Shaheed <mohas1203@wrdsb.ca>
2021-02-05 19:51:59 -0500

    set up templates with code blocks

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h1>Hello world</h1>"
+    return render_template("index.html")
+
+
+@app.route("/<name>")
+def user(name):
+    return f"hello {name}!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
